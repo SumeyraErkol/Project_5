121ce6f9f3958f3272e9f47fbd19d61512a1c363
LENOVO <sumeyra.erkol.tr.@gmail.com>
2023-02-04 23:28:26 +0300

    Initial commit

---
+++ src/production/Mortgage/StatesTax.java
@@ -0,0 +1,18 @@
+package production.Mortgage;
+
+public enum StatesTax {
+    NEWYORK(20),
+    NEWJERSEY(15),
+    FLORIDA(5),
+    OHIO(12);
+
+    private int tax;
+
+    private StatesTax(int tax) {
+        this.tax = tax;
+    }
+
+    public int getTax() {
+        return this.tax;
+    }
+}
